fa105f81a67508306c7709c1159ba033c6f45036
angelinawwu <angelinawwu@g.ucla.edu>
2025-04-21 09:56:49 -0700

    tailwind v4

---
+++ frontend/src/components/Home/DashboardHeader.tsx
@@ -0,0 +1,13 @@
+const DashboardHeader = () => {
+  return (
+    <div className="flex justify-between items-center px-4 mb-4">
+      <div className="flex items-center space-x-4">
+        <h1 className="text-2xl font-bold">
+          Dashboard
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHeader;
